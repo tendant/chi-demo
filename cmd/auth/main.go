2011f7acf9598f4f538586e97cd43463b8d11cd2
Lei Wang <tendant@gmail.com>
2022-10-02 23:29:56 -0700

    Add auth/main.go

---
+++ cmd/auth/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"log"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
+	"github.com/go-pkgz/auth"
+	"github.com/go-pkgz/auth/avatar"
+	"github.com/go-pkgz/auth/provider"
+	"github.com/go-pkgz/auth/token"
+)
+
+func main() {
+	// define options
+	options := auth.Opts{
+		SecretReader: token.SecretFunc(func(id string) (string, error) { // secret key for JWT
+			return "secret", nil
+		}),
+		TokenDuration:  time.Minute * 5, // token expires in 5 minutes
+		CookieDuration: time.Hour * 24,  // cookie expires in 1 day and will enforce re-login
+		Issuer:         "my-test-app",
+		URL:            "http://127.0.0.1:8080",
+		AvatarStore:    avatar.NewLocalFS("/tmp"),
+		Validator: token.ValidatorFunc(func(_ string, claims token.Claims) bool {
+			// allow only dev_* names
+			return claims.User != nil && strings.HasPrefix(claims.User.Name, "dev_")
+		}),
+	}
+
+	// create auth service with providers
+	service := auth.NewService(options)
+	service.AddDirectProvider("local", provider.CredCheckerFunc(func(user, password string) (ok bool, err error) {
+		ok, err = checkUserSomehow(user, password)
+		return ok, err
+	}))
+	// service.AddProvider("github", "<Client ID>", "<Client Secret>")   // add github provider
+	// service.AddProvider("facebook", "<Client ID>", "<Client Secret>") // add facebook provider
+
+	// retrieve auth middleware
+	m := service.Middleware()
+
+	// setup http server
+	router := chi.NewRouter()
+	router.Get("/open", openRouteHandler)                      // open api
+	router.With(m.Auth).Get("/private", protectedRouteHandler) // protected api
+
+	// setup auth routes
+	authRoutes, avaRoutes := service.Handlers()
+	router.Mount("/auth", authRoutes)  // add auth handlers
+	router.Mount("/avatar", avaRoutes) // add avatar handler
+
+	log.Fatal(http.ListenAndServe("localhost:8080", router))
+}
+
+func checkUserSomehow(user string, password string) (bool, error) {
+	log.Default()
+	if user == "user1" && password == "pass1" {
+		return true, nil
+	}
+	return false, errors.New("Incorrect username and/or password!")
+}
+
+func openRouteHandler(w http.ResponseWriter, r *http.Request) {
+	render.PlainText(w, r, "Open Route")
+}
+
+func protectedRouteHandler(w http.ResponseWriter, r *http.Request) {
+	render.PlainText(w, r, "*Protected Route*")
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,38 @@ go 1.18
 require github.com/go-chi/chi/v5 v5.0.7
 
 require (
+	cloud.google.com/go/compute v1.10.0 // indirect
 	github.com/ajg/form v1.5.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dghubble/oauth1 v0.7.1 // indirect
 	github.com/go-chi/render v1.0.2 // indirect
+	github.com/go-oauth2/oauth2/v4 v4.5.1 // indirect
+	github.com/go-pkgz/auth v1.19.1 // indirect
+	github.com/go-pkgz/repeater v1.1.3 // indirect
+	github.com/go-pkgz/rest v1.16.0 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/klauspost/compress v1.15.11 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.20 // indirect
+	github.com/montanaflynn/stats v0.6.6 // indirect
+	github.com/nullrocks/identicon v0.0.0-20180626043057-7875f45b0022 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.etcd.io/bbolt v1.3.6 // indirect
+	go.mongodb.org/mongo-driver v1.10.2 // indirect
+	golang.org/x/crypto v0.0.0-20220926161630-eccd6366d1be // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
+	golang.org/x/net v0.0.0-20221002022538-bcab6841153b // indirect
+	golang.org/x/oauth2 v0.0.0-20220909003341-f21342109be1 // indirect
+	golang.org/x/sync v0.0.0-20220929204114-8fcdb60fdcc0 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 )
