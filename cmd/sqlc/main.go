35bae77c8fe04859ff5fc591f9a35f37d9fd6465
Lei Wang <tendant@gmail.com>
2022-10-03 22:15:38 -0700

    add $ sqlc generate

---
+++ cmd/sqlc/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"log"
+	"reflect"
+
+	_ "github.com/lib/pq"
+	"github.com/tendant/chi-demo/tutorial"
+)
+
+func run() error {
+	ctx := context.Background()
+
+	db, err := sql.Open("postgres", "user=pqgotest dbname=pqgotest sslmode=verify-full")
+	if err != nil {
+		return err
+	}
+
+	queries := tutorial.New(db)
+
+	// list all authors
+	authors, err := queries.ListAuthors(ctx)
+	if err != nil {
+		return err
+	}
+	log.Println(authors)
+
+	// create an author
+	insertedAuthor, err := queries.CreateAuthor(ctx, tutorial.CreateAuthorParams{
+		Name: "Brian Kernighan",
+		Bio:  sql.NullString{String: "Co-author of The C Programming Language and The Go Programming Language", Valid: true},
+	})
+	if err != nil {
+		return err
+	}
+	log.Println(insertedAuthor)
+
+	// get the author we just inserted
+	fetchedAuthor, err := queries.GetAuthor(ctx, insertedAuthor.ID)
+	if err != nil {
+		return err
+	}
+
+	// prints true
+	log.Println(reflect.DeepEqual(insertedAuthor, fetchedAuthor))
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
