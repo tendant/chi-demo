9c2a6792a5305e5e421d532d1d9cb0f3f156a888
Lei Wang <tendant@gmail.com>
2022-10-05 02:12:51 -0700

    Add debugging middleware

--- cmd/auth/main.go
+++ cmd/auth/main.go
@@ -13,21 +13,25 @@ import (
 	"github.com/go-pkgz/auth/avatar"
 	"github.com/go-pkgz/auth/provider"
 	"github.com/go-pkgz/auth/token"
+	"github.com/tendant/chi-demo/middleware"
 )
 
 func main() {
 	// define options
 	options := auth.Opts{
+		SecureCookies: false,
+		DisableXSRF:   false, // when XSRF enabled, XSRF-TOKEN has to be presented with value from Cookie xsrf-token.
 		SecretReader: token.SecretFunc(func(id string) (string, error) { // secret key for JWT
-			return "secret", nil
+			return "ChangeMeToASecret", nil
 		}),
-		TokenDuration:  time.Minute * 5, // token expires in 5 minutes
-		CookieDuration: time.Hour * 24,  // cookie expires in 1 day and will enforce re-login
+		TokenDuration:  time.Minute * 60, // token expires in 5 minutes
+		CookieDuration: time.Hour * 24,   // cookie expires in 1 day and will enforce re-login
 		Issuer:         "my-test-app",
 		URL:            "http://127.0.0.1:8080",
 		AvatarStore:    avatar.NewLocalFS("/tmp"),
 		Validator: token.ValidatorFunc(func(_ string, claims token.Claims) bool {
 			// allow only dev_* names
+			log.Printf("claims user: %s.", claims.User.Name)
 			return claims.User != nil && strings.HasPrefix(claims.User.Name, "dev_")
 		}),
 	}
@@ -46,8 +50,10 @@ func main() {
 
 	// setup http server
 	router := chi.NewRouter()
-	router.Get("/open", openRouteHandler)                      // open api
-	router.With(m.Auth).Get("/private", protectedRouteHandler) // protected api
+	router.Use(middleware.LoggingMiddleware)
+	router.Get("/open", openRouteHandler)                                                    // open api
+	router.With(m.Auth, middleware.LoggingMiddleware).Get("/private", protectedRouteHandler) // protected api
+	// router.With(m.Trace).Get("/private", protectedRouteHandler) // protected api
 
 	// setup auth routes
 	authRoutes, avaRoutes := service.Handlers()
@@ -57,9 +63,11 @@ func main() {
 	log.Fatal(http.ListenAndServe("localhost:8080", router))
 }
 
+// curl -i http://localhost:8080/auth/local/login?user=user1&passwd=pass1
 func checkUserSomehow(user string, password string) (bool, error) {
+	log.Print(password)
 	log.Printf("Login with: %s, %s.", user, password)
-	if user == "user1" && password == "pass1" {
+	if user == "dev_user1" && password == "pass1" {
 		return true, nil
 	}
 	return false, errors.New("Incorrect username and/or password!")
