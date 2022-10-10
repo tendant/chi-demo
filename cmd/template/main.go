ab0b3a8ac84556858d9d79ecf74a5bd821c1d2be
Lei Wang <tendant@gmail.com>
2022-10-09 18:03:14 -0700

    add template program

---
+++ cmd/template/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	//...
+
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+
+	log.Print("Started server on localhost:3000...")
+	http.ListenAndServe("localhost:3000", r)
+}
