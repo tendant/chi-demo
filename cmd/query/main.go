89a52dfaabf4b34e0125e4f7059f9c727e44058e
Lei Wang <tendant@gmail.com>
2022-11-05 11:02:44 -0700

    add cmd/query/main.go

---
+++ cmd/query/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+
+	"github.com/ggicci/httpin"
+	"github.com/go-chi/chi/v5"
+)
+
+type ListUsersInput struct {
+	IsMember bool  `in:"query=is_member"`
+	AgeRange []int `in:"query=age_range[],age_range"`
+}
+
+func ListUsers(rw http.ResponseWriter, r *http.Request) {
+	input := r.Context().Value(httpin.Input).(*ListUsersInput)
+	fmt.Printf("input: %#v\n", input)
+}
+
+func main() {
+	router := chi.NewRouter()
+	router.With(
+		httpin.NewInput(ListUsersInput{}),
+	).Get("/users", ListUsers)
+
+	r, _ := http.NewRequest("GET", "/users?is_member=1&age_range=18&age_range=60", nil)
+
+	rw := httptest.NewRecorder()
+	router.ServeHTTP(rw, r)
+}
