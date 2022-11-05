7ef9ba14f3c3e72d756dcdd74257ea63aae488d7
Lei Wang <tendant@gmail.com>
2022-11-05 11:09:12 -0700

    update documents

--- cmd/query/main.go
+++ cmd/query/main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
+// Check document for more examples of directives
+// https://ggicci.github.io/httpin/directives/query
+// https://ggicci.github.io/httpin/directives/header
+// https://ggicci.github.io/httpin/directives/form
+// https://ggicci.github.io/httpin/directives/body
+// https://ggicci.github.io/httpin/directives/required
+// https://ggicci.github.io/httpin/directives/default
 type ListUsersInput struct {
 	IsMember bool  `in:"query=is_member"`
 	AgeRange []int `in:"query=age_range[],age_range"`
