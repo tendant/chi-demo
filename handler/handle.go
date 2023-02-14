bed1e70b0994737f4a8bb010bb6cb0b70da5c08f
Lei Wang <tendant@gmail.com>
2023-02-14 00:30:14 -0800

    add query params

--- handler/handle.go
+++ handler/handle.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/ggicci/httpin"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 	"go.uber.org/zap"
 )
 
@@ -17,6 +19,18 @@ func (handle *Handle) Demo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, Demo")
 }
 
+type QueryInput struct {
+	Q string `in:"query=q"`
+}
+
+func (handle *Handle) Query(w http.ResponseWriter, r *http.Request) {
+	handle.Log.Debug("Query...")
+	q := r.Context().Value(httpin.Input).(*QueryInput)
+	handle.Log.Debug("Q:", zap.Any("q", q))
+	render.PlainText(w, r, "Query")
+}
+
 func Routes(r *chi.Mux, handle Handle) {
 	r.Get("/demo", handle.Demo)
+	r.With(httpin.NewInput(QueryInput{})).Get("/query", handle.Query)
 }
