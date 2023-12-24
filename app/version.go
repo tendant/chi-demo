be500fc83312a3a5cbce366884d34600126565d9
Lei Wang <tendant@gmail.com>
2023-12-24 00:58:55 -0800

    add comment

--- app/version.go
+++ app/version.go
@@ -35,3 +35,14 @@ var Timestamp = func() string {
 
 	return "NoTimestamp"
 }()
+
+// for _, kv := range info.Settings {
+// 	switch kv.Key {
+// 	case "vcs.revision":
+// 		Revision = kv.Value
+// 	case "vcs.time":
+// 		LastCommit, _ = time.Parse(time.RFC3339, kv.Value)
+// 	case "vcs.modified":
+// 		DirtyBuild = kv.Value == "true"
+// 	}
+// }
