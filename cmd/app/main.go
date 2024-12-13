0b822f753672fc172ddeb8ba05f97c70d25b2b37
Lei Wang <tendant@gmail.com>
2024-12-12 22:43:15 -0800

    fix build errors

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"github.com/tendant/chi-demo/app"
-	"github.com/tendant/chi-demo/user"
 )
 
 func main() {
 	// newApp := app.Default()
 	server := app.DefaultApp()
-	var userImpl user.UserImpl
-	server.R.Mount("/", user.Handler(&userImpl))
 	server.Run()
 }
