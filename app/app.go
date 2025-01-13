a6d2e83ed469909796224349adcadf7880e20568
Lei Wang <tendant@gmail.com>
2025-01-13 15:59:22 -0800

    add CorsConfig

--- app/app.go
+++ app/app.go
@@ -68,19 +68,35 @@ func DefaultHttpLogger() *httplog.Logger {
 	return logger
 }
 
+type CorsConfig struct {
+	AllowedOrigins   []string
+	AllowedMethods   []string
+	AllowedHeaders   []string
+	ExposedHeaders   []string
+	AllowCredentials bool
+	MaxAge           int
+}
+
+func CustomCorsOptions(config CorsConfig) *cors.Options {
+	return &cors.Options{
+		AllowedOrigins:   config.AllowedOrigins,
+		AllowedMethods:   config.AllowedMethods,
+		AllowedHeaders:   config.AllowedHeaders,
+		ExposedHeaders:   config.ExposedHeaders,
+		AllowCredentials: config.AllowCredentials,
+		MaxAge:           config.MaxAge,
+	}
+}
+
 func DefaultCorsOptions() *cors.Options {
-	corsOptions := &cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		// AllowedOrigins: []string{"http://localhost:3000", "https://*.example.com"},
-		AllowedOrigins: []string{"https://*", "http://*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+	return CustomCorsOptions(CorsConfig{
+		AllowedOrigins:   []string{"https://*", "http://*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token", "X-API-KEY"},
 		ExposedHeaders:   []string{"Link"},
 		AllowCredentials: false,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}
-	return corsOptions
+		MaxAge:           300,
+	})
 }
 
 func NewApp(opts ...Option) *App {
